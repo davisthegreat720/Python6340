13ca7ec01a6735b967b4fad653d5751b9e91ec78
danielledavis <davisthegreat720@gmail.com>
2023-07-15 22:06:14 -0500

    Homework 2_1 Start

---
+++ Week2_1HMWK.py
@@ -0,0 +1,12 @@
+#i, j, and k are numbers
+#(a) i is 3, j is 5, and k is 7
+#(b) i is 3, j is 7, and k is 5
+
+i=int(input("Enter a number?"))
+j=int(input("Enter a Number?"))
+k=int(input("Enter another number"))
+
+if i<j:
+    if j<k:
+        i=j
+    e
