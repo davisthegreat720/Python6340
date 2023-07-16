7754359281e625e054251510c9b7929a95a77a5b
davisthegreat720 <davisthegreat720@gmail.com>
2023-07-16 17:47:53 -0500

    Week 2 Complete

---
+++ Week2_2HMWK.py
@@ -0,0 +1,26 @@
+# Explain what can be the output if x = 10 ,x=4,x=8
+
+x=int(input("enter a number?"))
+if x > 5:
+    if x == 6:
+        print("nested: x == 6")
+    elif x == 10:
+        print("nested: x == 10")
+    else:
+        print("nested: else")
+else:
+    print("something else")
+
+# b)
+
+# if x=10
+# enter a number? 10
+# nested: x == 10
+#
+# if x=4
+# enter a number? 4
+# something else
+#
+#if x=8
+#enter a number? 8
+#nested: else
