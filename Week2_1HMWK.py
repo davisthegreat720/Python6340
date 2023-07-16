f63cc805d1b0537bf5240dd7cf76d02ffde23646
davisthegreat720 <davisthegreat720@gmail.com>
2023-07-16 16:55:43 -0500

    Week 2 assignment 1 complete

--- Week2_1HMWK.py
+++ Week2_1HMWK.py
@@ -2,11 +2,31 @@
 #(a) i is 3, j is 5, and k is 7
 #(b) i is 3, j is 7, and k is 5
 
-i=int(input("Enter a number?"))
-j=int(input("Enter a Number?"))
-k=int(input("Enter another number"))
+i=int(input("Enter a number for i?"))
+j=int(input("Enter a Number for j?"))
+k=int(input("Enter another number k?"))
 
-if i<j:
-    if j<k:
-        i=j
-    e
+if i < j:
+    if j < k:
+        i = j
+    else:
+        j = k
+else:
+    if j > k:
+        j = i
+    else:
+        i = k
+
+print("i =", i, " j =", j, " k =", k)
+
+# a)
+# Enter a number for i? 3
+# Enter a Number for j? 5
+# Enter another number k? 7
+# i = 5  j = 5  k = 7
+
+# b)
+# Enter a number for i? 3
+# Enter a Number for j? 7
+# Enter another number k? 5
+# i = 3  j = 5  k = 5
