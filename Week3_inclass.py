b4bd51b98f794ce697ab71a4d3d7b5ff001a8e14
danielledavis <davisthegreat720@gmail.com>
2023-07-19 20:45:26 -0500

    Week 3 In class

---
+++ Week3_inclass.py
@@ -0,0 +1,15 @@
+print(" 'I like python' ")
+print(' "I like python" ')
+print('''I Like python''')
+#backslash is an escape character
+print('I\'m happy')
+print("I'M happy")
+#end put the next line with the same line as end
+print("Welcome to", end = ' ')
+print("My", "is", "Monty", "Python", end=' ')
+print("My", "is", "Monty", "Python", sep="_")
+
+print(1,2,2,4, sep="|")
+print('foo','bar',sep='&')
+print(1,2,3,4,end='!!!')
+print('ham','eggs''toast',sep='&',end="Yum!")
---
+++ week3_inclassLoop.py
@@ -0,0 +1,11 @@
+#
+
+#infinite loop
+d=False
+while not d:
+    entry=input("enter a value")
+    if entry == "*":
+        d==True
+        print(d)
+    else:
+        print(entry)
