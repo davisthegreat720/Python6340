eec5f1140b1f3757d879ca95c3853b806353535a
danielledavis <davisthegreat720@gmail.com>
2023-07-27 09:56:05 -0500

    Week 4 In class

---
+++ Week4InClass.py
@@ -0,0 +1,54 @@
+# word = input('Enter a word: ')
+# for letter in word:
+#     print(letter)
+import random
+# word = input('Enter a word: ')
+# for letter in word:
+#     print(letter,end='')
+
+# sum=0
+# entry=0
+# print('Entry numbers to sum, 99 to end the list:')
+# while True:
+#     entry=int(input('enter the number'))
+#     if entry == 99:
+#         break
+#     sum += entry
+# print ('sum=', sum)
+
+# sum = 0
+# done = False
+# while not done: #so long as done is false, val=1,2,3,999
+#     val = int(input('Enter postive integer (999 quits):'))
+#     if val < 0:
+#         print('Negative value', val, 'ignored')
+#         continue
+#     if val !=999:
+#         print('Tallying', val)
+#         sum += val
+#     else:
+#         done=(val==999)
+# print('sum=', sum)
+# print("done=",done)
+
+# n=int(input('enter and interger'))
+# if n < 1000:
+#     print('*',end='')
+# if n < 100:
+#     print('*',end='')
+# if n < 10:
+#     print('*', end='')
+# if n < 1:
+#     print('*', end='')
+
+#FUNCTIONS
+# from random import choice
+# def cointoss():
+#     options=['head','tail']
+#     result= choice(options)
+#     print(result)
+# cointoss()
+# cointoss()
+# cointoss()
+# cointoss()
+
